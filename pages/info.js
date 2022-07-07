3f865756e42e4ec398f25f4a1ae7e6276d1e737e
Edson <edson_a@outlook.com>
2022-07-07 19:43:01 -0300

    added info page

--- pages/info.js
+++ pages/info.js
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-export default function About() {
+export default function Info() {
   return (
     <div className={styles.container}>
-      <h1>Sobre</h1>
+      <h1>Info</h1>
 
       <hr></hr>
 
