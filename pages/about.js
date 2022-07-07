90c149b0dded1588ae8b7f6e0b5d6f17123b4471
Edson <edson_a@outlook.com>
2022-07-07 19:21:38 -0300

    first page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Meu app</h1>
+      <h1>Sobre</h1>
 
       <hr></hr>
 
-        <Link href='/about'>
-          About
+      
+        <Link href='/'>
+          Home
         </Link>
       
     </div>
